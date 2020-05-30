75ead82e65bd90582afa073a65a1af089e10b5c0
dschmidt555 <dschmitty5@gmail.com>
2020-05-30 09:11:08 -0700

    Magic 8 Ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,56 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Magic 8 Ball'),
+          backgroundColor: Colors.lightBlueAccent,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var randomArray = ['No', 'Yes', 'Maybe', 'I dont know', 'Heck No'];
+  int randomNum = 1;
+  int randomBall = 1;
+  var randomAnswer;
+
+  //This is where a function goes
+  void generateNum() {
+    setState(() {
+      randomNum = Random().nextInt(randomArray.length);
+      randomAnswer = randomArray[randomNum];
+      randomBall = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                generateNum();
+              },
+              child: Image.asset('images/ball$randomBall.png'),
+            ),
+          )
+        ],
       ),
     );
+  }
+}
